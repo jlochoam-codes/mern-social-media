2971064e8c090efa6686da54f05a2f852ed2aff6
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-22 12:40:07 -0500

    Added logo img

--- src/components/logoSearch/LogoSearch.jsx
+++ src/components/logoSearch/LogoSearch.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from '../../img/logo.png'
 import './LogoSearch.css'
 
 const LogoSearch = () => {
   return (
     <div className="logoSearch">
-      Logo Search
+      <img src={Logo} alt="Logo" />
     </div>
   )
 }
