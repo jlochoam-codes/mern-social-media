5107ebe3901433d97289453a71ecb7ead8a5b753
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-22 00:39:25 -0500

    Add blur decoration in background

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <div className="blur"></div>
-      <div className="blur"></div>
+      <div className="blur" style={{ top: '-18%', right: 0 }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
     </div>
   );
 }
