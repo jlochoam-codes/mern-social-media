90e3c54636995ee76a1fd928d67dac145a54adaf
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-22 13:26:47 -0500

    Added styled search bar and icon

--- src/components/logoSearch/LogoSearch.jsx
+++ src/components/logoSearch/LogoSearch.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { UilSearch } from '@iconscout/react-unicons'
 import Logo from '../../img/logo.png'
 import './LogoSearch.css'
 
 const LogoSearch = () => {
   return (
-    <div className="logoSearch">
+    <div className="logo">
       <img src={Logo} alt="Logo" />
+      <div className="search">
+        <input type="text"
+          name="searchBar"
+          placeholder='#Explore' />
+        <div className="searchIcon">
+          <UilSearch />
+        </div>
+      </div>
     </div>
   )
 }
