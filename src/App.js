f510d70661cfac3e7ffe77ffe03b48b0940670e1
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-22 00:30:28 -0500

    Add blur, text color, bg color, and padding to App class

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      App by Jose Luis Ochoa Monroy
+      <div className="blur"></div>
+      <div className="blur"></div>
     </div>
   );
 }
