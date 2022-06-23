29e68e27341ef15477e5c4f89b41299a290ecbd1
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-23 15:19:44 -0500

    Added ProfileCard component, used in ProfileSide

---
+++ src/components/profileCard/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProfileCard.css'
+
+const ProfileCard = () => {
+  return (
+    <div className='profileCard'>ProfileCard</div>
+  )
+}
+
+export default ProfileCard
--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LogoSearch from '../logoSearch/LogoSearch'
+import ProfileCard from '../profileCard/ProfileCard'
 import './ProfileSide.css'
 
 const ProfileSide = () => {
   return (
     <div className='profileSide'>
       <LogoSearch />
+      <ProfileCard />
     </div>
   )
 }
