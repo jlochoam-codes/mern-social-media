16325c5cb171a8a6a661f5e7e7cf67b2b5b0082a
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-24 19:21:36 -0500

    Styled ProfileCard component

--- src/components/profileCard/ProfileCard.jsx
+++ src/components/profileCard/ProfileCard.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css'
 
 const ProfileCard = () => {
   return (
-    <div className='profileCard'>ProfileCard</div>
+    <div className='profileCard'>
+
+      <div className='profileImages'>
+        <img src={Cover} alt="cover" />
+        <img src={Profile} alt="profile" />
+      </div>
+
+      <div className="profileName">
+        <span>Jose Ochoa</span>
+        <span>Web Developer</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="followers">
+            <span>9,999</span>
+            <span>Followers</span>
+          </div>
+          <div className="verticalLine" />
+          <div className="followings">
+            <span>256</span>
+            <span>Followings</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <span>My Profile</span>
+    </div>
   )
 }
 
