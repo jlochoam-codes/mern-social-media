3632a024165845973097a673efcc125aae92f524
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-24 22:13:53 -0500

    Added temporary data for layout design

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,27 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const followers = [
+  {
+    name: 'Andrew Thomas',
+    username: 'AThomas',
+    img: img1,
+  },
+  {
+    name: 'Hulk Buster',
+    username: 'HBuster',
+    img: img2,
+  },
+  {
+    name: 'Thunder Master',
+    username: 'Thor',
+    img: img3,
+  },
+  {
+    name: 'Natasha',
+    username: 'Natasha',
+    img: img4,
+  },
+];
