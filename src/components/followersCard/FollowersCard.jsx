b87be70912c2ba0552c9fba5483f1dcae24fa74d
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-06-25 00:17:04 -0500

    Styled FollowersCard component

--- src/components/followersCard/FollowersCard.jsx
+++ src/components/followersCard/FollowersCard.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { followers } from '../../Data/FollowersData'
 import './FollowersCard.css'
 
 const FollowersCard = () => {
   return (
-    <div className='followersCard'>FollowersCard</div>
+    <div className='followersCard'>
+      <h3>Who's following you</h3>
+      {
+        followers.map((follower, id) => {
+          return (
+            <div className="follower">
+              <div className="followerData">
+                <img src={follower.img} alt="follower profile" />
+                <div className="followerName">
+                  <span>{follower.name}</span>
+                  <span>@{follower.username}</span>
+                </div>
+              </div>
+              <input type="button" value="Follow" />
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
