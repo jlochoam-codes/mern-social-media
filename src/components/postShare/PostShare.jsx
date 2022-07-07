c3ce8825220b170d98da77c9deb7c2698d2a5681
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-07 00:41:08 -0500

    Styled PostShare component

--- src/components/postShare/PostShare.jsx
+++ src/components/postShare/PostShare.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import './PostShare.css'
 import ProfileImage from '../../img/profileImg.jpg'
+import {
+  UilScenery,
+  UilPlayCircle,
+  UilLocationPoint,
+  UilSchedule
+} from '@iconscout/react-unicons';
 
 const PostShare = () => {
   return (
-    <div className='PostShare'>
+    <div className="PostShare">
       <img src={ProfileImage} alt="Profile" />
-      <div className="Share">Share a post</div>
+      <div className="CreatePost">
+        <div className="postText">
+          <input type="text"
+            name="postBar"
+            placeholder="Say it to the world!" />
+        </div>
+        <div className="postOptions">
+          <div className="option" style={{ color: "var(--photo)" }}>
+            <UilScenery />
+            <span className="description">Photo</span>
+          </div>
+          <div className="option" style={{ color: "var(--video)" }}>
+            <UilPlayCircle />
+            <span className="description">Video</span>
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <UilLocationPoint />
+            <span className="description">Location</span>
+          </div>
+          <div className="option" style={{ color: "var(--schedule)" }}>
+            <UilSchedule />
+            <span className="description">Schedule</span>
+          </div>
+          <div className="submitPost">
+            <input type="button" value="Share" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
