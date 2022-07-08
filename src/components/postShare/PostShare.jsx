7f7d4dd4db5e82a61d87b0a7f932a10f2491e83f
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-08 13:15:17 -0500

    Styled PostShare component and added 'attach photo' functionality

--- src/components/postShare/PostShare.jsx
+++ src/components/postShare/PostShare.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './PostShare.css'
 import ProfileImage from '../../img/profileImg.jpg'
 import {
   UilScenery,
-  UilPlayCircle,
   UilLocationPoint,
-  UilSchedule
+  UilSchedule,
+  UilTimes
 } from '@iconscout/react-unicons';
 
 const PostShare = () => {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+
+  const onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img)
+      });
+    }
+  }
+
+  const cancelImagePost = () => {
+    setImage(null);
+  }
+
   return (
     <div className="PostShare">
       <img src={ProfileImage} alt="Profile" />
@@ -19,13 +35,11 @@ const PostShare = () => {
             placeholder="Say it to the world!" />
         </div>
         <div className="postOptions">
-          <div className="option" style={{ color: "var(--photo)" }}>
+          <div className="option" style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
             <UilScenery />
             <span className="description">Photo</span>
-          </div>
-          <div className="option" style={{ color: "var(--video)" }}>
-            <UilPlayCircle />
-            <span className="description">Video</span>
           </div>
           <div className="option" style={{ color: "var(--location)" }}>
             <UilLocationPoint />
@@ -38,7 +52,24 @@ const PostShare = () => {
           <div className="submitPost">
             <input type="button" value="Share" />
           </div>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              accept="image/*"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
+        {image && (
+          <div className="previewImage">
+            <span onClick={cancelImagePost}>
+              <UilTimes />
+            </span>
+            <img src={image.image} alt="For new post" />
+          </div>
+        )}
       </div>
     </div>
   )
