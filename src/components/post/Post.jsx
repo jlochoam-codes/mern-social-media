7ccee49598bf8d4bd494fe715bc64e60ae08d7b6
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-11 23:06:31 -0500

    Styled Post component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Post.css'
+import Like from '../../img/like.png'
+// import NotLike from '../../img/notlike.png'
+import Comment from '../../img/comment.png'
+import Share from '../../img/share.png'
+import PostImage from '../../img/postpic1.jpg'
 
 const Post = () => {
   return (
-    <div className='Post'>Post</div>
+    <div className='Post'>
+      <div className="PostImage">
+        <img src={PostImage} alt="Post" />
+      </div>
+      <div className="PostContent">
+        <div className="PostUser">Tzuyu</div>
+        <div className="PostText">Estoy #AprendiendoEspanol</div>
+      </div>
+      <div className="PostLikes">2300 likes</div>
+      <div className="PostInteraction">
+        <img src={Like} alt="Liked post, click to unlike" />
+        <img src={Comment} alt="Click to comment on post" />
+        <img src={Share} alt="Click to share post" />
+      </div>
+    </div>
   )
 }
 
