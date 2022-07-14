45451a0289a0a2fc19a4c268d9edf005d451a775
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-14 15:57:10 -0500

    Styled Profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import ProfileCard from '../../components/profileCard/ProfileCard'
 import ProfileInfo from '../../components/profileInfo/ProfileInfo'
+import TrendSide from '../../components/trendSide/TrendSide'
 import './Profile.css'
 
 const Profile = () => {
   return (
     <div className='Profile'>
       <ProfileInfo />
+      <ProfileCard />
+      <TrendSide />
     </div>
   )
 }
