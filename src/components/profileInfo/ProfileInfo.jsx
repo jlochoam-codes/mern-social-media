22aa7c412a05aaf63d1fe1d33d186590d1ee5827
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-14 15:49:33 -0500

    Added ProfileInfo component, used by Profile page

---
+++ src/components/profileInfo/ProfileInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileInfo = () => {
+  return (
+    <div className='ProfileInfo'>ProfileInfo</div>
+  )
+}
+
+export default ProfileInfo
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ProfileInfo from '../../components/profileInfo/ProfileInfo'
 import './Profile.css'
 
 const Profile = () => {
   return (
-    <div className='Profile'>Profile</div>
+    <div className='Profile'>
+      <ProfileInfo />
+    </div>
   )
 }
 
