083b5383519d6c89a8c35d410cb154d1ba40163c
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-14 14:00:12 -0500

    Styled TrendSide component

--- src/components/trendSide/TrendSide.jsx
+++ src/components/trendSide/TrendSide.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './TrendSide.css'
+import Trends from '../trends/Trends'
+import Home from '../../img/home.png'
+import { UilSetting } from '@iconscout/react-unicons'
+import Notif from '../../img/noti.png'
 
 const TrendSide = () => {
   return (
-    <div className='trendSide'>Trend</div>
+    <div className='trendSide'>
+      <div className="ActionBar">
+        <img src={Home} alt="Home button" />
+        <img src={Notif} alt="Notifications button" />
+        <div className="option">
+          <UilSetting size="1.5rem" />
+        </div>
+      </div>
+      <Trends />
+    </div>
   )
 }
 
