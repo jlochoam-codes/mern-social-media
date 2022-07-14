d60a9cfece5bfefbd2f986e1e078b198cf9419e8
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-14 14:17:17 -0500

    Styled Trends component

--- src/components/trends/Trends.jsx
+++ src/components/trends/Trends.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Trends.css'
+import Trend from '../trend/Trend'
+import { TrendsData } from '../../Data/TrendsData'
 
 const Trends = () => {
   return (
-    <div className='Trends'>Trends</div>
+    <div className='Trends'>
+      <div className="TrendsTitle">Trends for you</div>
+      {
+        TrendsData.map((trend, id) => {
+          return <Trend data={trend} id={id} />
+        })
+      }
+    </div>
   )
 }
 
