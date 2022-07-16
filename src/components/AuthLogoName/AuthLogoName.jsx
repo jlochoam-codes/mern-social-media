f5366d2ee01e4856240c6b1a3a77ab2a5faa5b9d
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-15 20:28:42 -0500

    Styled AuthLogoName component

--- src/components/AuthLogoName/AuthLogoName.jsx
+++ src/components/AuthLogoName/AuthLogoName.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './AuthLogoName.css'
+import Logo from '../../img/logo.png'
 
 const AuthLogoName = () => {
   return (
-    <div className='AuthLogoName'>AuthLogoName</div>
+    <div className='AuthLogoName'>
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="Name">
+        <h1>FaceTweet</h1>
+        <h6>I hope you have fun in my social media website!</h6>
+      </div>
+    </div>
   )
 }
 
