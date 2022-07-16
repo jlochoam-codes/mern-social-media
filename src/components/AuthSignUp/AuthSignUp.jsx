5f5daddd2b6e774491e7db83182390c37d47b47a
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-07-15 22:09:29 -0500

    Added AuthSignUp component, used by Auth page

---
+++ src/components/AuthSignUp/AuthSignUp.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './AuthSignUp.css'
+
+const AuthSignUp = () => {
+  return (
+    <div className='AuthSignUp'>
+      <div className="AuthSignUpHeader">
+        <h4>Sign up</h4>
+      </div>
+      <div className="AuthSignUpForm">
+        <div className="Name">
+          <div className="FirstName">
+            <input type="text"
+              name="firstName"
+              placeholder="First Name"
+              autoFocus />
+          </div>
+          <div className="LastName">
+            <input type="text"
+              name="lastName"
+              placeholder="Last Name" />
+          </div>
+        </div>
+        <div className="Username">
+          <input type="text"
+            name="username"
+            placeholder="Username" />
+        </div>
+        <div className="Password">
+          <div className="Pw">
+            <input type="text"
+              name="password"
+              placeholder="Password" />
+          </div>
+          <div className="ConfirmPw">
+            <input type="text"
+              name="confirmPw"
+              placeholder="Confirm Password" />
+          </div>
+        </div>
+      </div>
+      <div className="AuthSignUpFooter">
+        <div className="ToLogin">
+          <a href="/">Already have an account? Login</a>
+        </div>
+        <div className="AuthSignUpSubmit">
+          <input type="button" value="Sign Up" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AuthSignUp
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import AuthLogoName from '../../components/AuthLogoName/AuthLogoName'
+import AuthSignUp from '../../components/AuthSignUp/AuthSignUp'
 import './Auth.css'
 
 const Auth = () => {
   return (
     <div className='Auth'>
       <AuthLogoName />
-      <div>Auth</div>
+      <AuthSignUp />
     </div>
   )
 }
