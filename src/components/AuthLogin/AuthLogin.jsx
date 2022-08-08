f2ee760d3bdf0ebeca6ba60b2057b2a4ab090ea7
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-08 00:19:18 -0500

    Updated AuthLogin and AuthSignUp to confirm same pw and launch POST request on submit

--- src/components/AuthLogin/AuthLogin.jsx
+++ src/components/AuthLogin/AuthLogin.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthLogin.css'
 
 const AuthLogin = ({ setIsSignUp }) => {
+  const [loginData, setLoginData] = useState({
+    username: "", password: ""
+  });
+
+  const handleFieldChange = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='AuthLogin'>
       <div className="AuthLoginHeader">
         <h4>Login</h4>
       </div>
-      <div className="AuthLoginForm">
+      <form className="AuthLoginForm" onSubmit={handleSubmit} method='post'>
         <div className="Username">
           <input type="text"
             name="username"
             placeholder="Username"
-            autoFocus />
+            autoFocus
+            required
+            onChange={handleFieldChange} />
         </div>
         <div className="Pw">
           <input type="password"
             name="password"
-            placeholder="Password" />
+            placeholder="Password"
+            required
+            onChange={handleFieldChange} />
         </div>
-      </div>
-      <div className="AuthLoginFooter">
-        <div className="ToSignUp">
-          <button type='button'
-            onClick={() => { setIsSignUp(true) }}>No account yet? Signup</button>
+        <div className="AuthLoginFooter">
+          <div className="ToSignUp">
+            <button type='button'
+              onClick={() => { setIsSignUp(true) }}>No account yet? Signup</button>
+          </div>
+          <div className="AuthLoginSubmit">
+            <button type="submit" >Login</button>
+          </div>
         </div>
-        <div className="AuthLoginSubmit">
-          <input type="button" value="Login" />
-        </div>
-      </div>
+      </form>
     </div>
   )
 }
--- src/components/AuthSignUp/AuthSignUp.jsx
+++ src/components/AuthSignUp/AuthSignUp.jsx
@@ -1,53 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthSignUp.css'
 
 const AuthSignUp = ({ setIsSignUp }) => {
+  const [signUpData, setSignUpData] = useState({
+    firstName: "", lastName: "", username: "", password: "", confirmPw: ""
+  });
+
+  const [isSamePw, setIsSamePw] = useState(true);
+
+  const handleFieldChange = (e) => {
+    // If updating pw after confirmation error, remove the error
+    if (e.target.name === 'password' || e.target.name === 'confirmPw') setIsSamePw(true);
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (signUpData.password !== signUpData.confirmPw) {
+      setIsSamePw(false);
+      return;
+    }
+  };
+
   return (
     <div className='AuthSignUp'>
       <div className="AuthSignUpHeader">
         <h4>Sign up</h4>
       </div>
-      <div className="AuthSignUpForm">
+      <form className="AuthSignUpForm" onSubmit={handleSubmit} method='post' >
         <div className="Name">
           <div className="FirstName">
             <input type="text"
               name="firstName"
               placeholder="First Name"
-              autoFocus />
+              autoFocus
+              required
+              value={signUpData.firstName}
+              onChange={handleFieldChange} />
           </div>
           <div className="LastName">
             <input type="text"
               name="lastName"
-              placeholder="Last Name" />
+              placeholder="Last Name"
+              required
+              value={signUpData.lastName}
+              onChange={handleFieldChange} />
           </div>
         </div>
         <div className="Username">
           <input type="text"
             name="username"
-            placeholder="Username" />
+            placeholder="Username"
+            required
+            value={signUpData.username}
+            onChange={handleFieldChange} />
         </div>
         <div className="Password">
           <div className="Pw">
             <input type="password"
               name="password"
-              placeholder="Password" />
+              placeholder="Password"
+              required
+              value={signUpData.password}
+              onChange={handleFieldChange} />
           </div>
           <div className="ConfirmPw">
             <input type="password"
               name="confirmPw"
-              placeholder="Confirm Password" />
+              placeholder="Confirm Password"
+              required
+              value={signUpData.confirmPw}
+              onChange={handleFieldChange} />
           </div>
         </div>
-      </div>
-      <div className="AuthSignUpFooter">
-        <div className="ToLogin">
-          <button type='button'
-            onClick={() => { setIsSignUp(false) }}>Already have an account? Login</button>
+        <div className="AuthSignUpPwConfirmation"
+          style={{
+            display: isSamePw ? 'none' : 'block',
+            color: 'red',
+            fontSize: '0.8rem',
+            alignSelf: 'flex-end',
+          }}>
+          * Confirm Password is not the same
         </div>
-        <div className="AuthSignUpSubmit">
-          <input type="button" value="Sign Up" />
+        <div className="AuthSignUpFooter">
+          <div className="ToLogin">
+            <button type='button'
+              onClick={() => { setIsSignUp(false) }}>Already have an account? Login</button>
+          </div>
+          <div className="AuthSignUpSubmit">
+            <button type="submit">Sign Up</button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
