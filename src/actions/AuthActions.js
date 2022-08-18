48f62aa7ee04fcf7f7579ceb2f2edb78bc02bbfe
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-17 22:12:36 -0500

    Changed {response} to {data} to match the object name used by authApi to save the response

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -3,8 +3,8 @@ import * as authApi from '../api/AuthReqs';
 export const login = (loginData) => async (dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const { response } = await authApi.login(loginData);
-    dispatch({ type: "LOGIN_SUCCESS", data: response });
+    const { data } = await authApi.login(loginData);
+    dispatch({ type: "LOGIN_SUCCESS", data: data });
   } catch (err) {
     console.error(err);
     dispatch({ type: "LOGIN_FAIL" });
@@ -14,8 +14,8 @@ export const login = (loginData) => async (dispatch) => {
 export const signUp = (signUpData) => async (dispatch) => {
   dispatch({ type: "SIGNUP_START" });
   try {
-    const { response } = await authApi.signUp(signUpData);
-    dispatch({ type: "SIGNUP_SUCCESS", data: response });
+    const { data } = await authApi.signUp(signUpData);
+    dispatch({ type: "SIGNUP_SUCCESS", data: data });
   } catch (err) {
     console.error(err);
     dispatch({ type: "SIGNUP_FAIL" });
