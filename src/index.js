6f814f5859d6db6468140f4c4aff95826a187951
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-21 00:52:21 -0500

    Added React Router DOM to show home page only when user is logged in or signed up, show auth page otherwise

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/ReduxStore";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
