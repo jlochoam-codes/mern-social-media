d7558f850ab1faa8a859483d14b9ed99acca24b1
Jose Luis Ochoa Monroy <joseluis-ochoamonroy@outlook.com>
2022-08-26 23:57:40 -0500

    Removed import of no longer existing file PostsData.js from Posts component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { PostsData } from '../../Data/PostsData'
 import { useSelector } from 'react-redux'
 import './Posts.css'
 import Post from '../post/Post'
